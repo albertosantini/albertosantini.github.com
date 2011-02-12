259e2fe03a12b5c8c5d6ccc0b41b089cb1592b64
icebox <albertosantini@gmail.com>
2011-02-12 11:26:24 +0100

    jQuery for scraping and new tagcloug module.

--- examples/crema/crema.js
+++ examples/crema/crema.js
@@ -1,87 +1,84 @@
 /*jslint browser: true */
-/*globals YUI */
+/*globals YUI, jQuery */
 
-
-YUI().use('node', 'async-queue', 'yql', 'gallery-tagcloud', function (Y) {
+YUI({
+    filter: 'raw'
+}).use('node', 'async-queue', 'yql', 'gallery-tagcloud',
+    function (Y) {
     var yuiForumUrl = "http://yuilibrary.com/forum/",
         yuiRecentPostsUrl = "http://yuilibrary.com/forum/recent.php",
         goButton = Y.one("#go"),
         forumEdit = Y.one("#forum"),
-        authors = [], tags = [];
-
-    function init() {
-        forumEdit.set('value', yuiRecentPostsUrl);
-        goButton.set("disabled", false);
-    }
+        authors = [], tags = [], totalTopics = 0, counterTopics = 0;
 
     function getTopics(url, cbfunc) {
         var topics = [];
 
         Y.YQL('select * from html where url="' + url + '"', function (r) {
-            var n, i, result = r.query.results;
+            jQuery(".tablebg tr td a[href*=viewtopic]", r.results[0])
+                .each(function () {
+                    topics.push(jQuery(this).attr("href"));
+            });
 
-            n = result.body.div.div.div[2].div[0].div.table[1].tr.length;
-            for (i = 1; i < n; i += 1) { // first element is the header
-                topics.push(result.body.div.div.div[2].div[0].div
-                    .table[1].tr[i].td[0].p.a.href);
-            }
             cbfunc(topics);
-        });
+        }, {format: 'xml'});
     }
 
     function createClouds() {
-        var text1 = Y.one("#text1"),
-            text2 = Y.one("#text2"),
-            authorsWithoutSpaces = [];
+        var text1 = Y.one("#text1"), text2 = Y.one("#text2");
 
         text1.empty(true);
         text2.empty(true);
 
-        Y.each(authors, function (author) {
-            author = author.replace(/\W/g, '_');
-            authorsWithoutSpaces.push(author);
+        text1.set('innerHTML', authors);
+        text1.tagCloud("#cloud1", {
+            sort: false,
+            fieldDelimeter: ',',
+            fieldRegex: new RegExp(/[ ]+/g)
         });
-        text1.set('innerHTML', authorsWithoutSpaces);
-        text1.tagCloud("#cloud1", {sort: false});
         text2.set('innerHTML', tags);
-        text2.tagCloud("#cloud2", {sort: false});
+        text2.tagCloud("#cloud2", {
+            sort: false,
+            fieldDelimeter: ',',
+            fieldRegex: new RegExp(/[ ]+/g)
+        });
     }
 
     function getPostInfo(url) {
         var q = this.pause();
 
         Y.YQL('select * from html where url="' + url + '"', function (r) {
-            var n, i, m, j, result;
+            jQuery(".tablebg .mod h3", r.results[0])
+                .each(function () {
+                    authors.push(jQuery(this).text().replace(/[ ]+/g, '_'));
+            });
 
-            if (r.query.count > 0) {
-                result = r.query.results;
-                n =  result.body.div.div.div[2].div[0].div.div[1].div[1].table.length;
-                for (i = 0; i < n; i += 1) {
-                    if (result.body.div.div.div[2].div[0].div.div[1].div[1].table[i].tr.td[0].div.length > 0) {
-                        authors.push(result.body.div.div.div[2].div[0].div.div[1].div[1].table[i].tr.td[0].div[0].h3);
-                        m = result.body.div.div.div[2].div[0].div.div[1].div[1].table[i].tr.td[0].div[1].ul.li.length;
-                        for (j = 0; j < m; j += 1) {
-                            tags.push(result.body.div.div.div[2].div[0].div.div[1].div[1].table[i].tr.td[0].div[1].ul.li[j].a.content);
-                        }
-                    } else {
-                        authors.push(result.body.div.div.div[2].div[0].div.div[1].div[1].table[i].tr.td[0].div.h3);
-                    }
-                }
-            }
+            jQuery(".tablebg .tags ul li", r.results[0])
+                .each(function () {
+                    tags.push(jQuery(this).text()
+                        .trim().replace(/[ ]+/g, '').replace(/\W/g, '_'));
+            });
+
+            counterTopics += 1;
+            goButton.set('text', counterTopics + '/' + totalTopics + ' topics');
 
             createClouds();
 
             q.run();
-        });
+        }, {format: 'xml'});
     }
 
-    function forumScraping(url) {
-        getTopics(url, function (topics) {
-            var i,
-                n = topics.length,
-                q = new Y.AsyncQueue();
+    goButton.on("click", function (e) {
+        goButton.set("disabled", true);
+        goButton.set('text', 'Loading topics...');
 
-            for (i = 0; i < n; i += 1) {
+        getTopics(forumEdit.get('value'), function (topics) {
+            var i, q = new Y.AsyncQueue();
+
+            totalTopics = topics.length;
+            goButton.set('text', '0/' + totalTopics + ' topics');
+
+            for (i = 0; i < totalTopics; i += 1) {
                 q.add({
                     fn: getPostInfo,
                     args: [yuiForumUrl + topics[i]]
@@ -89,25 +86,14 @@ YUI().use('node', 'async-queue', 'yql', 'gallery-tagcloud', function (Y) {
             }
 
             q.add(function () {
+                goButton.set('text', 'Search');
                 goButton.set("disabled", false);
             });
 
             q.run();
         });
-    }
-
-    goButton.on("click", function (e) {
-        var forumUrl = forumEdit.get('value');
-
-        this.set("disabled", true);
-        try {
-            forumScraping(forumUrl);
-        } catch (ex) {
-            init();
-        }
     });
 
-    init();
+    forumEdit.set('value', yuiRecentPostsUrl);
+    goButton.set("disabled", false);
 });
-
-
