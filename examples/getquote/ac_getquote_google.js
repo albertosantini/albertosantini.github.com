320ce01e7edb4749c54fe6010c235e54a611752d
icebox <albertosantini@gmail.com>
2013-08-24 17:23:11 +0200

    getquote - Refactor scripts.

---
+++ examples/getquote/ac_getquote_google.js
@@ -0,0 +1,28 @@
+/*global YUI, alert */
+
+YUI({
+    filter: "raw"
+}).use("autocomplete", function (Y) {
+    "use strict";
+
+    var acNode = Y.one("#ac-input");
+
+    acNode.plug(Y.Plugin.AutoComplete, {
+        maxResults: 10,
+        resultTextLocator: "t",
+        resultListLocator: "query.results.json.matches",
+        resultFormatter: function (query, results) {
+            return Y.Array.map(results, function (result) {
+                var asset = result.raw;
+
+                return asset.t + " - " + asset.n + " - " + asset.e;
+            });
+        },
+        source: "select * from json" +
+            "where url=\"http://www.google.com/finance/match?q={query}\""
+    });
+
+    acNode.ac.on("select", function (e) {
+        alert(e.result.raw.n);
+    });
+});
--- examples/getquote/ac_getquote_yahoo.js
+++ examples/getquote/ac_getquote_yahoo.js
@@ -48,4 +48,3 @@ YUI({
         alert(e.result.raw.name);
     });
 });
-
