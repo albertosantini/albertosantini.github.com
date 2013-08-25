96189a6bbfbd1521080bd3c4ca8372988d1ff161
icebox <albertosantini@gmail.com>
2013-08-25 09:45:08 +0200

    getquote - Wait Twitter Typeahead 0.10.

--- examples/getquote/ac_getquote_typeahead.js
+++ examples/getquote/ac_getquote_typeahead.js
@@ -7,48 +7,53 @@ YAHOO.Finance.SymbolSuggest = {};
 (function ($) {
     "use strict";
 
-    // var assets = {};
 
-    YAHOO.Finance.SymbolSuggest.ssCallback = function (data) {
-        // var res;
-
-        // res = $.map(data.ResultSet.Result, function (item) {
-        //     var asset = {
-        //             symbol: item.symbol,
-        //             name: item.name,
-        //             type: item.type,
-        //             exchDisp: item.exchDisp
-        //         },
-        //         key = asset.symbol + " " + asset.name +
-        //             " (" + asset.type + " - " + asset.exchDisp + ")";
-
-        //     assets[key] = asset;
-
-        //     return key;
-        // });
-
-        // process(res);
-
-        console.log(data);
-    };
 
     $("#ac-input").typeahead({
         name: "symbols",
         limit: 10,
         remote: {
-            url: "http://autoc.finance.yahoo.com/autoc?query=%QUERY",
-            replace: function (url) {
-                console.log(url);
+            url: "http://autoc.finance.yahoo.com/autoc?",
+            dataType: "jsonp",
+            cache: true,
+            replace: function (url, uriEncodedQuery) {
+                var that = this;
+
+                YAHOO.Finance.SymbolSuggest.ssCallback = function (data) {
+                    var res;
+
+                    res = $.map(data.ResultSet.Result, function (item) {
+                        var asset = {
+                                symbol: item.symbol,
+                                name: item.name,
+                                type: item.type,
+                                exchDisp: item.exchDisp
+                            },
+                            key = asset.symbol + " " + asset.name +
+                                " (" + asset.type + " - " + asset.exchDisp + ")";
+
+                        return key;
+                    });
+
+                    console.log("callback:", res);
+
+                    that.filter(res);
+                };
 
                 return url +
+                    "query=" + uriEncodedQuery +
                     "&callback=YAHOO.Finance.SymbolSuggest.ssCallback";
             },
             filter: function (response) {
-                console.log(response);
+                console.log("response:", response);
 
                 return response;
             }
         }
+    });
+
+    $("#ac-input").on("typeahead:autocompleted", function (e) {
+        console.log("completed:", e);
     });
 
 }(jQuery));
